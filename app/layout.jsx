d4c8f264e15062327e49d871f45463480fa4ef07
Carlos <carlosbarriosf6@gmail.com>
2025-11-28 09:28:44 +0100

    added Vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
